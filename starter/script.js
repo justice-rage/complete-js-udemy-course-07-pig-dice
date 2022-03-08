013e5c745ba972ea7a3476f6c8819465d1852df4
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-08 15:38:18 -0800

    complete lecture 84

--- starter/script.js
+++ starter/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // selecting elements
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
 const current0Element = document.querySelector('#current--0');
 const current1Element = document.querySelector('#current--1');
-
 const diceElement = document.querySelector('.dice');
+
 const buttonNew = document.querySelector('.btn--new');
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
@@ -15,7 +17,10 @@ score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+// starting conditions
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // rolling dice functionality
 buttonRoll.addEventListener('click', function() {
@@ -29,9 +34,12 @@ buttonRoll.addEventListener('click', function() {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    current0Element.textContent = currentScore; // change later
-
+    document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
   } else {
     // switch to next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 })
