560b0959e072203c641874caa6d631cd017c858c
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-08 14:38:34 -0800

    complete lecture 82

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// selecting elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+const diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
